a0ad227d2d435b7073cfa85b7a022bf61d90ed57
heisenbergye <heisenbergye@outlook.com>
2020-02-27 15:55:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,6 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.252/g' package/base-files/files/bin/config_generate
-sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_generate
-sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
 mkdir package/my
 git clone https://github.com/Leo-Jo-My/luci-app-ssr-plus-Jo package/my/luci-app-ssr-plus-Jo
 ./scripts/feeds update -a
